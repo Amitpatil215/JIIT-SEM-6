35c7708cc289272efa59c8835e25fe2aa11b234f
Amit Patil <amitgpatil215@gmail.com>
2022-06-08 02:08:31 +0530

    added assignments

---
+++ Problem_solving_lab/Lab9/q1.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define debug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+
+template <typename Arg1>
+void __f(const char *name, Arg1 &&arg1)
+{
+    cout << name << " : " << arg1 << endl;
+}
+
+template <typename Arg1, typename... Args>
+void __f(const char *names, Arg1 &&arg1, Args &&...args)
+{
+    const char *comma = strchr(names + 1, ',');
+    cout.write(names, comma - names) << " : " << arg1 << " | ";
+    __f(comma + 1, args...);
+}
+
+void solve()
+{
+}
+
+int main()
+{
+    solve();
+    return 0;
+}
